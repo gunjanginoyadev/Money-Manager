fbc6bfcce7b9f1b268664c071cffd25ce6a9492f
gunjanginoyadev <gunjan.ginoya.dev@gmail.com>
2026-03-27 19:45:39 +0530

    Update app_env.dart

--- lib/core/config/app_env.dart
+++ lib/core/config/app_env.dart
@@ -2,14 +2,14 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppEnv {
-  static String get firebaseApiKey => dotenv.env['FIREBASE_API_KEY'] ?? '';
-  static String get firebaseAppId => dotenv.env['FIREBASE_APP_ID'] ?? '';
+  static String get firebaseApiKey => dotenv.env['FIREBASE_API_KEY'] ?? 'AIzaSyAlc85KFZ2FOZwAx6XGJ6OhCNx9n9v01sQ';
+  static String get firebaseAppId => dotenv.env['FIREBASE_APP_ID'] ?? '1:854909411412:web:def3def3d02069609a4dc1';
   static String get firebaseMessagingSenderId =>
-      dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '';
-  static String get firebaseProjectId => dotenv.env['FIREBASE_PROJECT_ID'] ?? '';
-  static String get firebaseAuthDomain => dotenv.env['FIREBASE_AUTH_DOMAIN'] ?? '';
+      dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '854909411412';
+  static String get firebaseProjectId => dotenv.env['FIREBASE_PROJECT_ID'] ?? 'money-manager-6aa55';
+  static String get firebaseAuthDomain => dotenv.env['FIREBASE_AUTH_DOMAIN'] ?? 'money-manager-6aa55.firebaseapp.com';
   static String get firebaseStorageBucket =>
-      dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '';
+      dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? 'money-manager-6aa55.firebasestorage.app';
   static String get firebaseMeasurementId =>
       dotenv.env['FIREBASE_MEASUREMENT_ID'] ?? '';
 
